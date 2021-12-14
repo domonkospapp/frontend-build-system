02ed2cad3223fcc173d16814edd0028cff34284a
Domonkos Papp <pappdomonkos96@gmail.com>
2021-12-14 22:40:17 +0100

    Update rollup to use ts

--- rollup.config.js
+++ rollup.config.js
@@ -1,8 +1,10 @@
 import typescript from "@rollup/plugin-typescript"
+import alias from '@rollup/plugin-alias';
+import { nodeResolve } from '@rollup/plugin-node-resolve';
 
 export default [
     {
-        input: "src/ts/app.ts",
+        input: "src/ts/app.tsx",
         output: {
             dir: "public/js",
             format: "esm",
@@ -11,7 +13,16 @@ export default [
         plugins:[
             typescript({
                 tsconfig: "./tsconfig.json"
-            })
+            }),
+            alias({
+                entries: [
+                    { find: 'react', replacement: 'preact/compat' },
+                    { find: 'react-dom/test-utils', replacement: 'preact/test-utils' },
+                    { find: 'react-dom', replacement: 'preact/compat' },
+                    { find: 'react/jsx-runtime', replacement: 'preact/jsx-runtime' }
+                ]
+            }),
+            nodeResolve()
         ]
         
     }
---
+++ src/ts/app.tsx
@@ -0,0 +1,4 @@
+import { h, render } from 'preact';
+import HomePage from './HomePage';
+
+render((<HomePage name="zucc" />), document.body);
